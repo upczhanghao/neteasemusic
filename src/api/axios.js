e312a40bd0e7830f044df7c39541091fe70d7fe4
ZhangHao <upc.zh324@gmail.com>
2020-05-25 13:08:39 +0800

    api请求地址修复

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://192.168.1.183:3000/'
+axios.defaults.baseURL = 'http://192.168.1.143:3000/'
 axios.defaults.timeout = 5000
 // 封装get/post等方法
 export default {
